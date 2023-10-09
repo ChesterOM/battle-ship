32afb8b4152652f7c779cba1da6886a87fe2a2a0
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-10-09 18:09:07 +1300

    Add intial gameboard tests

--- tests/classes.test.js
+++ tests/classes.test.js
@@ -20,3 +20,25 @@ describe('ship testing', () => {
         ship.hit()
     })
 })
+
+describe('gameboard testing', () => {
+    test('places a ship at specified coordinates', () => {
+        const gameboard = new Gameboard();
+        gameboard.placeShip(3, [1, 1]);
+        // Add more assertions depending on how I store ships
+    });
+
+    test('records a hit or miss when attacked', () => {
+        const gameboard = new Gameboard();
+        gameboard.placeShip(2, [1, 1], [1, 2]);
+        expect(gameboard.receiveAttack([1, 1])).toBe('hit');
+        expect(gameboard.receiveAttack([2, 2])).toBe('miss');
+    });
+
+    test('identifies when all ships have been sunk', () => {
+        const gameboard = new Gameboard();
+        gameboard.placeShip(1, [1, 1]);
+        gameboard.receiveAttack([1, 1]);
+        expect(gameboard.allShipsSunk()).toBe(true);
+    });
+})
