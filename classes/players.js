ef1273d51c5d6a94fbaea5579e6dbd5fe37bffc9
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2023-10-10 21:57:37 +1300

    Add player module

---
+++ classes/players.js
@@ -0,0 +1,42 @@
+const { GameBoard } = require('./gameboard.js');
+const { Ship } = require('./ships.js');
+
+
+class Player {
+    constructor(type = 'human'){
+        this.gameboard = new GameBoard();
+        this.type = type;
+    }
+
+    placeShip(ship, coord, orientation) {
+        this.gameboard.placeShip(ship, coord, orientation);
+    }
+
+    attack(gameboard, coord) {
+        return this.gameboard.receiveAttack(coord);
+    }
+
+    allShipsSunk() {
+        return this.gameboard.allShipsSunk()
+    }
+
+    randomCoordinate(boardSize){
+        const x = Math.floor(Math.random() * boardSize);
+        const y = Math.floor(Math.random() * boardSize);
+        return [x,y];
+    }
+
+    computerAttack(opponentGameboard){
+        let coord;
+        do {
+            coord = this.randomCoordinate(opponentGameboard.board.length);
+        } while (opponentGameboard.board[coord[0]][coord[1]] === 'miss' || 
+                opponentGameboard.board[coord[0]][coord[1]] instanceof Ship &&
+                typeof opponentGameboard.board[coord[0]][coord[1]] === 'number'
+                );
+        return this.attack(opponentGameboard, coord);
+    }
+
+}
+
+module.exports.Player = Player;
